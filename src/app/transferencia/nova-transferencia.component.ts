dd114a40fd7277f64e13b8c7e700817fa1f5d03e
jeffersonfreitas21 <jefferson.dev21@gmail.com>
2022-03-18 10:16:40 -0300

    Output values

--- src/app/transferencia/nova-transferencia.component.ts
+++ src/app/transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output} from "@angular/core";
 
 @Component({
     selector: 'app-nova-transferencia',
@@ -8,10 +8,19 @@ import { Component } from "@angular/core";
 
 export class NovaTransferenciaComponent {
 
+  @Output() aoTransferir = new EventEmitter<any>();
+
   valor: number
   destino: number
 
   transferir(){
-    alert("Nova transferencia realizada - " + this.valor +" - "+ this.destino)
+    const valorEmitir = {valor: this.valor, destino: this.destino}
+    this.aoTransferir.emit(valorEmitir)
+    this.limparCampos();
+  }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
   }
 }
